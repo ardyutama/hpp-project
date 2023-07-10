9ef498a5cd2e708243795df1ac2293bc3dbaacaf
ardyutama <ardy.putra@pinhome.id>
2023-07-10 21:46:22 +0700

    refactor: change path

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/calculate',
+      path: '/',
       name: 'calculate',
       component: CalculateView
     }
